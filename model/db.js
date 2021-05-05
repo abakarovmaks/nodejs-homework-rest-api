4420bb2e6c88f92d5a368a3641521959f13f8080
abakarovmaks <abakarovmaks@gmail.com>
2021-05-05 15:25:53 +0300

    Adds mongoose connection

--- model/db.js
+++ model/db.js
@@ -1,9 +1,33 @@
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const adapter = new FileSync('./model/contacts.json');
-const db = low(adapter);
+const uriDb = process.env.URI_DB;
 
-db.defaults({ contacts: [] }).write();
+const db = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  poolSize: 5,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Database connection successful');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(`Mongoose error: ${err.message}`);
+  process.exit(1);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected from DB');
+});
+
+process.on('SIGINT', async () => {
+  mongoose.connection.close(() => {
+    console.log('Connection to DB closed');
+    process.exit(1);
+  });
+});
 
 module.exports = db;
